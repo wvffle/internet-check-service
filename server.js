113a61ad2456a11ae15310b2439d67e32fba9adb
Kasper Seweryn <github@wvffle.net>
2020-07-02 11:46:26 +0000

    Add server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const fastify = require('fastify')()
+const mime = require('mime')
+
+const { promises: fs, constants: { R_OK } } = require('fs')
+
+fastify.register(require('fastify-metrics'), { endpoint: '/__metrics' })
+
+fastify.get('/', async (request, reply) => {
+  const accept = request.headers['Accept']
+  
+  if (accept) {
+    if (accept === 'text/none') {
+      return ''
+    }
+
+    if (accept.endsWith('/json')) {
+      return { wyjebalo: 'nie' }
+    }
+  }
+
+  reply.type('html')
+  return fs.readFile(`${__dirname}/dist/index.html`)
+})
+
+fastify.get('/*', async ({ req: { originalUrl } }, reply) => {
+  const queryIndex = originalUrl.indexOf('?')
+  const file = originalUrl.slice(0, queryIndex === -1 ? Infinity : queryIndex)
+  reply.type(mime.getType(file.slice(file.lastIndexOf('.'))))
+  return fs.readFile(`${__dirname}/dist${file}`)
+})
+
+
+fastify.listen({
+  port: process.env.PORT || 8083,
+  host: '0.0.0.0'
+})
